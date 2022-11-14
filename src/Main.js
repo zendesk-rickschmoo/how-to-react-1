1ce74d1b64bf3fcc4fa436dcf4d91ffb2d0138fa
Richard Boardman <85193226+zendesk-rickschmoo@users.noreply.github.com>
2022-11-14 13:39:12 -0800

    globalalert working

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Main } from '@zendeskgarden/react-chrome';
 import { Button } from '@zendeskgarden/react-buttons';
+import { Notification, Title } from '@zendeskgarden/react-notifications';
+import { GlobalAlert, IGlobalAlertProps } from '@zendeskgarden/react-notifications';
+import { Anchor } from '@zendeskgarden/react-buttons';
 import { DEFAULT_THEME } from '@zendeskgarden/react-theming';
 import Modal from './Modal';
 
@@ -8,9 +11,41 @@ export default () => {
   const [isModalVisible, setModalVisible] = useState(false);
 
   return (
-    <Main style={{ padding: DEFAULT_THEME.space.md }}>
+
+    <div>
+    <Notification>
+    <Title>Example Title</Title>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+  </Notification>
+   {/* <GlobalAlertStory
+      anchor="Anchor"
+      ariaLabel="Label"
+      button="Button"
+      content="Content"
+      hasClose
+      title="Title"
+      type="info"
+    /> */}
+  <GlobalAlert type="info">
+    <GlobalAlert.Content>
+      <Title>
+          Title
+      </Title>
+      "Content"
+      <Anchor href="#">
+          Anchor
+       </Anchor>
+    </GlobalAlert.Content>
+   <GlobalAlert.Button>Button 123</GlobalAlert.Button>
+   <GlobalAlert.Close aria-label="test123" />
+  </GlobalAlert>
+  
+  <Main style={{ padding: DEFAULT_THEME.space.md }}>
       <Button onClick={() => setModalVisible(true)}>Sign in</Button>
       {isModalVisible && <Modal onClose={() => setModalVisible(false)} />}
+
     </Main>
-  );
+
+    </div>
+);
 };
